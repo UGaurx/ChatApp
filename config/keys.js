7825e3238a27ed1e9e041e3ddb2ea475c74ff09f
UTKARSH GAUR <ugaurx@gmail.com>
2025-07-03 13:18:50 +0530

    Fixed security leak

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb+srv://shwetarx:Shweta1234@cluster0.hiiy0cy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  mongoURI: "mongodb+srv://<your-username>:<your-password>@<cluster-url>",
   secretOrKey: "secret",
 };
